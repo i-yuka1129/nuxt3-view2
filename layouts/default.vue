c312d77da4d6ad9270e88d7fc188cefea1464556
i-yuka1129 <y.izumisawa1129@gmail.com>
2024-05-29 14:14:16 +0900

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <slot />
+    <footer>&copy; proglus.jp</footer>
+  </div>
+</template>
+
+<style>
+  .container {
+    color: red;
+  }
+
+  footer {
+    background-color: orange;
+    color: white;
+    text-align: center;
+  }
+</style>
